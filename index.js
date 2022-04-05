22fc897162d7d982919a0da60b82220eb588cf16
jnhenkel <jeffnhenkel@gmail.com>
2022-04-05 15:58:23 -0700

    json middleware

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4002;
 
+//middlewares
+app.use(express.json());
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
-})
+});
