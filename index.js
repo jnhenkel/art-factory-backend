4b079a87cc4180889f20b09276c01378d8a01ec4
jnhenkel <jeffnhenkel@gmail.com>
2022-04-08 17:05:26 -0700

    get root update

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const port = process.env.PORT || 4002;
 //middlewares
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.status(200).json({done: true, message: 'This is the backend for the art factory'});
+});
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
